94b0aef930148b2a2e46f6cd732ede04feecb0f3
cameron-bret <c.bret@whiskerhead.com>
2019-12-11 13:13:46 -0500

    footer basic in place

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-
-import A from 'components/A';
-import LocaleToggle from 'containers/LocaleToggle';
 import Wrapper from './Wrapper';
-import messages from './messages';
 
 function Footer() {
   return (
     <Wrapper>
       <section>
-        <FormattedMessage {...messages.licenseMessage} />
+        <p>(888) 864-1232</p>
       </section>
       <section>
-        <LocaleToggle />
-      </section>
-      <section>
-        <FormattedMessage
-          {...messages.authorMessage}
-          values={{
-            author: <A href="https://twitter.com/mxstbr">Max Stoiber</A>,
-          }}
-        />
+        <p>Copywright 2020 Pet Power Radio</p>
       </section>
     </Wrapper>
   );
